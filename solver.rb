ecca45b6e30a0496bc82c0aae18f3655862e8220
eroiyo <59938389+eroiyo@users.noreply.github.com>
2021-10-21 07:29:11 -0400

    solve factorial problem

--- solver.rb
+++ solver.rb
@@ -2,5 +2,18 @@
 
 # Book Element
 class Solver
+    def self.factorial(num)
+        return 'error' if num.negative?
+        return 1 if num.zero?
+    
+        if num.positive?
+          result = 1
+          while num.positive?
+            result = num * result
+            num -= 1
+          end
+          result
+        end
+      end
 end
   
